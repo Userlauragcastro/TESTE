ac1e39d4f10f8f4c3f59eb73359400441e094dd2
Userlauragcastro <86833360+Userlauragcastro@users.noreply.github.com>
2021-11-18 11:07:55 -0300

    Add files via upload

---
+++ aula 7/config.js
@@ -0,0 +1,15 @@
+let ano_atual=2021;
+
+let nome=prompt('digite_seu_nome');
+
+
+let ano_de_nascimento= parseInt(prompt('digite seu ano de ano_de_nascimento'));
+
+let idade= ano_atual - ano_de_nascimento;
+
+
+
+alert(`a idade de  ${nome} é ${idade}`);
+
+
+alert(`  Olá, seu nome é  `  +  nome  +   ` e você tem  `+ idade + ` anos  `)  ;
